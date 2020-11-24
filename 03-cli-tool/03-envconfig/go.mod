e7555bdceb130d0018dd86204ad74ac11b5f3162
Bo-Yi Wu <appleboy.tw@gmail.com>
2020-11-25 00:00:28 +0800

    chore(03): add envconfig

---
+++ 03-cli-tool/03-envconfig/go.mod
@@ -0,0 +1,8 @@
+module foobar
+
+go 1.15
+
+require (
+	github.com/joho/godotenv v1.3.0
+	github.com/kelseyhightower/envconfig v1.4.0
+)
---
+++ 03-cli-tool/03-envconfig/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/joho/godotenv"
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Server struct {
+	Debug      bool
+	Port       int
+	User       string
+	Users      []string
+	Rate       float32
+	Timeout    time.Duration
+	ColorCodes map[string]int
+}
+
+func main() {
+	var envfile string
+	flag.StringVar(&envfile, "env-file", ".env", "Read in a file of environment variables")
+	flag.Parse()
+
+	godotenv.Load(envfile)
+	cfg := &Server{}
+	err := envconfig.Process("app", cfg)
+	if err != nil {
+		log.Fatal("invalid configuration")
+	}
+	fmt.Println(cfg)
+}
